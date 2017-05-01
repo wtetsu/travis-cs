c640429c7984b31b8904d49222bc94901bf0fa60
渡邉 徹彦 <tewatana@gmail.com>
2017-05-01 12:11:46 +0900

    Add initial solution

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public int Plus(int val1, int val2)
+        {
+            return val1 + val2;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var class1 = new Class1();
+            Assert.Equals(2, class1.Plus(1, 1));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var class1 = new Class1();
+            Assert.Equals(0, class1.Plus(0, 0));
+        }
+
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var class1 = new Class1();
+            Assert.Equals(30, class1.Plus(10, 20));
+        }
+    }
+}
